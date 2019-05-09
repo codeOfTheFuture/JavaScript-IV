b2cf3547ec2b07ec383d25936c00767ca17fdbcf
Jeff Oliver <jeffoliver3384@gmail.com>
2019-05-09 16:06:43 -0400

    WIP: lambda-classes.js completed Person and Instructor classes working on student and project manager classes next

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,33 @@
 // CODE here for your Lambda Classes
+
+// Person class
+class Person {
+  constructor(attributes) {
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+  }
+
+  speak() {
+    console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+  }
+}
+
+// Instructor class
+class Instructor extends Person {
+  constructor(attributes) {
+    super(attributes);
+
+    this.specialty = attributes.specialty;
+    this.favLanguage = attributes.favLanguage;
+    this.catchPhrase = attributes.catchPhrase;
+  }
+
+  demo(subject) {
+    console.log(`Today we are learning about ${subject}`);
+  }
+
+  grade(student, subject) {
+    console.log(`${student.name} receives a perfect score on ${subject}`);
+  }
+}
